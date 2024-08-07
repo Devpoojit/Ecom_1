b35c820e4d89b57e7721b0ed40d7d21d317bc730
Devpoojit Verma <devpoojit10912@gmail.com>
2024-08-07 18:17:59 +0530

    !== error

--- src/Full.jsx
+++ src/Full.jsx
@@ -13,7 +13,7 @@ export default function Full({ product, onClose }) {
           <h2>{product.title}</h2>
           <p className="text-muted">{product.category}</p>
           <h4 className="text-success strike-through "><del>₹{product.price}</del><span className="text-danger">    {product.discountPercentage}% Off!</span></h4>
-          <h4 className="text-success">₹{product.discountPercentage != 0 ? (product.price - product.price*product.discountPercentage/100).toPrecision(3) : product.price}</h4>
+          <h4 className="text-success">₹{product.discountPercentage !== 0 ? (product.price - product.price*product.discountPercentage/100).toPrecision(3) : product.price}</h4>
           <p>{product.description}</p>
           <button className="btn btn-warning me-2">Add to Cart</button>
           <button className="btn btn-danger ml-2" onClick={onClose}>Close</button>
